be817cca51cc60a489c5d2691960bae4e00d3b1c
yejin4117 <s2108@e-mirim.hs.kr>
2023-08-29 13:48:42 +0900

    Students 클래스 구현

---
+++ DelegatorStudent32/Students.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatorStudent32
+{
+    internal class Students
+    {
+        private List<Student> listOfStudent = new List<Student>();
+        public delegate void PrintProcess(Student s);
+
+        public void Add(Student s)
+        {
+            listOfStudent.Add(s);
+        }
+
+        public void Print(PrintProcess process)
+        {
+            foreach (Student s in listOfStudent)
+            {
+                process(s);
+            }
+        }
+
+        public void Print()
+        {
+            Print((s) => Console.WriteLine(s));
+            /*
+            Print(delegate (Student s)
+            {
+                Console.WriteLine(s);
+            });*/
+        }
+
+    }
+}
