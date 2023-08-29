06ccf0cf66da227cfce4bcc088247dfcfdd9cd46
yejin4117 <s2108@e-mirim.hs.kr>
2023-08-29 13:36:29 +0900

    Student 클래스 추가

---
+++ DelegatorStudent32/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatorStudent32
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+
+        public Student(string name, double score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Score;
+        }
+    }
